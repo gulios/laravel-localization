9b94777426e617d1f3fcdfb970a3c040704f49ce
Gulios <gulios@gulios.pl>
2025-12-07 23:40:13 +0100

    Fix PHP 8.4 deprecation: str_replace null value

--- src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -648,7 +648,11 @@ class LaravelLocalization
             elseif ($value instanceOf UrlRoutable) {
                 $value = $value->getRouteKey();
             }
-            $route = str_replace(array('{'.$key.'}', '{'.$key.'?}'), $value, $route);
+            $route = str_replace(
+                ['{' . $key . '}', '{' . $key . '?}'],
+                (string) ($value ?? ''),
+                $route
+            );
         }
 
         // delete empty optional arguments that are not in the $attributes array
